ddf1aa6cc081956a6e2e9c10b22bcd2b490ebb04
Sitdev <desarrollotic3@ttncompany.com>
2024-12-27 08:48:48 -0500

    Removing log

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,6 @@ const DB_HOST = env("DB_HOST");
 const DB_PORT = env("DB_PORT");
 const DATABASE_URI = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
 
-console.log("Testing pre commit");
-
 async function bootstrap(): Promise<void> {
     const appRoutes = routes();
     const server = new AppServer({
